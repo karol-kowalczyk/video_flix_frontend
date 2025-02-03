dcd8e7a59d29b62b10395299566d4d399feadd12
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2025-02-03 13:40:33 +0100

    added an main page component, where the user can decide if he wants to login or signup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { MainSideComponent } from './main-side/main-side.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'', redirectTo: 'main', pathMatch: 'full' },
+    { path:'main', component: MainSideComponent}
+];
---
+++ src/app/main-side/main-side.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-side',
+  imports: [],
+  templateUrl: './main-side.component.html',
+  styleUrl: './main-side.component.scss'
+})
+export class MainSideComponent {
+
+}
