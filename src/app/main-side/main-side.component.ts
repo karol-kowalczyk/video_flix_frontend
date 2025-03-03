ad9ff385b73424cd9b12ccb74297d0e971db6ed7
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2025-03-03 20:18:00 +0100

    added the imprint to the main side of the application

--- src/app/main-side/main-side.component.ts
+++ src/app/main-side/main-side.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FooterComponent } from "../shared/footer/footer.component";
 
 
 @Component({
   selector: 'app-main-side',
-  imports: [RouterLink],
+  imports: [RouterLink, FooterComponent],
   templateUrl: './main-side.component.html',
   styleUrl: './main-side.component.scss'
 })
